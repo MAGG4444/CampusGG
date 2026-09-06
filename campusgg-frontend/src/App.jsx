def4609af1ad8fcf7413cb8c3960fa6e1d6dd004
MAGG4444 <hjingwen645@gmail.com>
2026-09-06 18:09:05 -0400

    F-Frontend-1-Set_up_React_frontend_structure

---
+++ campusgg-frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="app">
+      <h1>CampusGG</h1>
+      <p>React frontend setup complete.</p>
+    </div>
+  )
+}
+
+export default App
