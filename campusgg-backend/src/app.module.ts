b14501e6f0d4baf113d2452b1d6a4c029ec5949e
Jingwen Huang <hjingwen645@gmailM.com>
2026-09-12 22:48:05 -0400

    lint fix 2

---
+++ campusgg-backend/src/app.module.ts
@@ -0,0 +1,11 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { LobbiesController } from './lobbies/lobbies.controller';
+
+@Module({
+  imports: [],
+  controllers: [AppController, LobbiesController],
+  providers: [AppService],
+})
+export class AppModule {}
