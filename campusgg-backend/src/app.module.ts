710aefc76d8ebb80668f369f3a3bcb3201314b2d
JustinSamuel522 <74522289+JustinSamuel522@users.noreply.github.com>
2026-09-13 17:56:44 -0400

    updated our frontend to match with the API (i forgot to push the previous one from last sem

--- campusgg-backend/src/app.module.ts
+++ campusgg-backend/src/app.module.ts
@@ -1,4 +1,10 @@
 import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { LobbiesController } from './lobbies/lobbies.controller';
 
-@Module({})
+@Module({
+  controllers: [AppController, LobbiesController],
+  providers: [AppService],
+})
 export class AppModule {}
