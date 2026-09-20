28746e57fc100ac3d1ce2f3b48ddc912179f441e
Jingwen Huang <hjingwen645@gmailM.com>
2026-09-20 18:53:18 -0400

    F-Signin-1-addsigninpage

--- campusgg-frontend/src/App.jsx
+++ campusgg-frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Layout from './components/Layout.jsx'
+import { AuthProvider } from './context/AuthContext.jsx'
 import { JoinedLobbiesProvider } from './context/JoinedLobbiesContext.jsx'
 import Chat from './pages/Chat.jsx'
 import CreateLobby from './pages/CreateLobby.jsx'
@@ -11,26 +12,30 @@ import Lobby from './pages/Lobby.jsx'
 import LobbyDetail from './pages/LobbyDetail.jsx'
 import PostDetail from './pages/PostDetail.jsx'
 import Profile from './pages/Profile.jsx'
+import SignIn from './pages/SignIn.jsx'
 
 function App() {
   return (
-    <JoinedLobbiesProvider>
-      <BrowserRouter>
-        <Routes>
-          <Route element={<Layout />}>
-            <Route path="/" element={<Home />} />
-            <Route path="/lobby" element={<Lobby />} />
-            <Route path="/lobby/:lobbyId" element={<LobbyDetail />} />
-            <Route path="/create-lobby" element={<CreateLobby />} />
-            <Route path="/forum" element={<Forum />} />
-            <Route path="/forum/:postId" element={<PostDetail />} />
-            <Route path="/create-post" element={<CreatePost />} />
-            <Route path="/chat" element={<Chat />} />
-            <Route path="/profile" element={<Profile />} />
-          </Route>
-        </Routes>
-      </BrowserRouter>
-    </JoinedLobbiesProvider>
+    <AuthProvider>
+      <JoinedLobbiesProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route element={<Layout />}>
+              <Route path="/" element={<Home />} />
+              <Route path="/lobby" element={<Lobby />} />
+              <Route path="/lobby/:lobbyId" element={<LobbyDetail />} />
+              <Route path="/create-lobby" element={<CreateLobby />} />
+              <Route path="/forum" element={<Forum />} />
+              <Route path="/forum/:postId" element={<PostDetail />} />
+              <Route path="/create-post" element={<CreatePost />} />
+              <Route path="/chat" element={<Chat />} />
+              <Route path="/profile" element={<Profile />} />
+              <Route path="/signin" element={<SignIn />} />
+            </Route>
+          </Routes>
+        </BrowserRouter>
+      </JoinedLobbiesProvider>
+    </AuthProvider>
   )
 }
 
